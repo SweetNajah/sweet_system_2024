db5db2bef23f9f68dfbe725cd956f4cd8b5bc983
AliDawood55 <s12144413@stu.najah.edu>
2024-08-09 20:39:49 +0300

    update the code

---
+++ src/main/java/sweet_2024/Product.java
@@ -0,0 +1,20 @@
+package sweet_2024;
+
+public class Product {
+    private String name;
+    private int unitsSold;
+    private double revenue;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getUnitsSold() {
+        return unitsSold;
+    }
+
+    public double getRevenue() {
+        return revenue;
+    }
+}
+
